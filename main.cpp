cd565c56e9da0cddf4ca0d844a4bc6dd860c41c1
ibrahim-ibrahim <folahanmi0001@gmail.com>
2024-07-14 20:46:31 +0100

    logical operators, loops

--- main.cpp
+++ main.cpp
@@ -49,5 +49,45 @@ int main() {
 
     sum(10, 20);
 
+    // Floating Point Format Specifiers
+    double an = 6.0221409e23;
+    printf("Avogadro's Number: %lev %lfw %lgx\n", an, an, an);
+    float hp = 9.75;
+    printf("Hogwarts' Platform: %e %f %g\n", hp, hp, hp);
+
+    char x = 'M';
+    wchar_t y = L'Z';
+    printf("Windows binaries start with %c%lc.\n", x, y);
+
+    // Comparison Operators
+    printf(" 7 == 7: %du\n", 7 == 7);
+    printf(" 7 != 7: %d\n", 7 != 7);
+    printf("10 > 20: %d\n", 10 > 20);
+    printf("10 >= 20: %d\n", 10 >= 20);
+    printf("10 < 20: %d\n", 10 < 20);
+    printf("20 <= 20: %d\n", 20 <= 20);
+
+    bool t = true;
+    bool f = false;
+    printf("!true: %d\n", !t);
+    printf("true && false: %d\n", t && f);
+    printf("true && !false: %d\n", t && !f);
+    printf("true || false: %d\n", t || f);
+    printf("false || false: %d\n", f || f);
+
+    // Arrays
+    int arr[] = { 1, 2, 3, 4 }; // can omit the length of the array
+    printf("The third element is %d.\n", arr[2]); // access the third element
+    arr[2] = 100; // update the third element
+    printf("The third element is %d.\n", arr[2]);
+
+    // Loops
+    unsigned long maximum = 0;
+    unsigned long values[] = { 10, 50, 20, 40, 0 };
+    for(size_t i=0; i < 5; i++) {
+        if (values[i] > maximum) maximum = values[i];
+    }
+    printf("The maximum value is %lu", maximum);
+
     return 0; // this is an exit code, a 0 means the program ran successfully, others codes might indicate a problem
 }
